37a7ce659c1d302572d1df20d08d44f0ebed6fc6
ayoub aitbelhaj <aitbelhajayoub566@example.com>
2022-11-10 12:48:47 +0100

    ss

--- Makefile
+++ Makefile
@@ -1,18 +1,18 @@
 name = .done
 up: $(name)
 $(name):
-	@docker volume create --name wordpress
-	@touch .done
 	@cd srcs && docker-compose up -d
 run:
 	@cd srcs && docker-compose up
 build :
 	@cd srcs && docker-compose build
-clean:
-	@docker system prune -a -f
-	@docker volume rm 
-	@docker image rm $(docker images -q)
-	@rm -f .done
 
+rmi:
+	@docker image rm --force $(docker image ls -q)
+rmv:
+	@docker volume rm $(docker volume ls -q)
+rmc:
+	@docker container rm $(docker contianer ls -q)
+clean : rmc rmv rmi
 rebuild : clean build
 	$echo "LOL"
--- srcs/requirements/wordpress/tools/wpscript.sh
+++ srcs/requirements/wordpress/tools/wpscript.sh
@@ -1,8 +1,8 @@
-wp core is-installed --allow-root
-if [ $? -eq 0 ]
-then
+# wp core is-installed --allow-root
+# if [ $? -eq 0 ]
+# then
 wp core download --allow-root
-fi
+# fi
 wp core config --dbname=$DB_NAME --dbuser=$WP_USER --dbhost=db --dbpass=$DB_PASSWORD --allow-root 2> /dev/null
-wp core install --url=aaitbelh.42.fr --title="wordpress" --admin_user="$WP_USER" --admin_password="$WP_PASSWORD" --admin_email="$WP_EMAIL" --allow-root 2> /dev/null
+wp core install --url=10.12.177.168 --title="wordpress" --admin_user="$WP_USER" --admin_password="$WP_PASSWORD" --admin_email="$WP_EMAIL" --allow-root 2> /dev/null
 php-fpm7.3 --nodaemonize
